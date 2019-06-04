b9275ee079c75b723f8eb429fb585dccc95c39cc
onejeet <jitendra909@gmail.com>
2019-06-04 10:17:43 +0530

    method name changed

--- src/Component/GameItem.js
+++ src/Component/GameItem.js
@@ -6,7 +6,7 @@ import {updateCurrentGame} from '../Actions';
 
 class GameItem extends Component{
 
-    setRedirect = () => {
+    setGame = () => {
         this.props.updateCurrentGame(this.props.game);
         return null;
     }
@@ -15,7 +15,7 @@ class GameItem extends Component{
         return (
             <tr>
                     <td>{game.Rank}</td>
-                    <td className="title"> <Link to="/GamePage"><span onClick={this.setRedirect}>{game.Name}<i className="fa fa-external-link-square" aria-hidden="true"></i></span></Link></td>
+                    <td className="title"> <Link to="/GamePage"><span onClick={this.setGame}>{game.Name}<i className="fa fa-external-link-square" aria-hidden="true"></i></span></Link></td>
                     <td>{game.Platform}</td>
                     <td>{game.Year}</td>
                     <td>{game.Genre}</td>
