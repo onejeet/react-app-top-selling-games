aec8e1dc9e0af5d9922f2f6524ffabb6502bdb49
onejeet <jitendra909@gmail.com>
2019-07-08 16:31:09 +0530

    Updates

---
+++ precache-manifest.7efb16c26c3bd05919a0107f6c248850.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d88507bf2eb4d8f431b0e0cd615477",
+    "url": "top-selling-games-react/index.html"
+  },
+  {
+    "revision": "528802146f2f9574dd8c",
+    "url": "top-selling-games-react/static/css/2.60583969.chunk.css"
+  },
+  {
+    "revision": "c996f26fc39f592d37bb",
+    "url": "top-selling-games-react/static/css/main.a22a6231.chunk.css"
+  },
+  {
+    "revision": "528802146f2f9574dd8c",
+    "url": "top-selling-games-react/static/js/2.963d95af.chunk.js"
+  },
+  {
+    "revision": "c996f26fc39f592d37bb",
+    "url": "top-selling-games-react/static/js/main.04d3a6cd.chunk.js"
+  },
+  {
+    "revision": "ba30211fcbb90b6e430d",
+    "url": "top-selling-games-react/static/js/runtime~main.40b9419d.js"
+  },
+  {
+    "revision": "09f178b22aaaae8acc6f81f462f07355",
+    "url": "top-selling-games-react/static/media/data.09f178b2.csv"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "top-selling-games-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "top-selling-games-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "top-selling-games-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "top-selling-games-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "top-selling-games-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
