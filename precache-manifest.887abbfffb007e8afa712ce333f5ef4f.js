c542ce93dc638e5e685fcbfa0f82ef22a434bb0c
onejeet <jitendra909@gmail.com>
2019-07-08 17:03:34 +0530

    Updates

---
+++ precache-manifest.887abbfffb007e8afa712ce333f5ef4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1692057e9f773abe22d57a8c0044f4d3",
+    "url": "/react-app-top-selling-games/index.html"
+  },
+  {
+    "revision": "6e307ef864b4fb4fbaa0",
+    "url": "/react-app-top-selling-games/static/css/2.25ab13a1.chunk.css"
+  },
+  {
+    "revision": "22c4dea9d41251c3fd48",
+    "url": "/react-app-top-selling-games/static/css/main.a22a6231.chunk.css"
+  },
+  {
+    "revision": "6e307ef864b4fb4fbaa0",
+    "url": "/react-app-top-selling-games/static/js/2.963d95af.chunk.js"
+  },
+  {
+    "revision": "22c4dea9d41251c3fd48",
+    "url": "/react-app-top-selling-games/static/js/main.4c30a208.chunk.js"
+  },
+  {
+    "revision": "8320799fde3f63790703",
+    "url": "/react-app-top-selling-games/static/js/runtime~main.1a2f851e.js"
+  },
+  {
+    "revision": "6e72876973a44fde42f13957ace8ce9a",
+    "url": "/react-app-top-selling-games/static/media/data.6e728769.csv"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app-top-selling-games/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app-top-selling-games/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app-top-selling-games/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app-top-selling-games/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app-top-selling-games/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c000c7d1a7b7f74e3d6a6244b3fa8c21.js"
+  "/react-app-top-selling-games/precache-manifest.887abbfffb007e8afa712ce333f5ef4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-top-selling-games/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
